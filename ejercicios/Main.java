2f47a0316ab94d51ec8083d0e59c3b892b58a7cd
martin <mm389358@gmail.com>
2023-06-23 20:21:00 -0300

    solved the first three exercises

---
+++ ejercicios/Main.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+public class Main {
+    public static void main(String[] args){
+        Function function = new Function();
+
+        print(function.sumOfMultiplesOf_3_or_5(1000));
+        print(function.sumOfMultiplesOf_3_or_5(1,1000));
+
+        print(function.evenFibonacciNumbers(0,1,4000000));
+
+        print(function.largestPrimeFactorOf(600851475143L));
+
+
+
+    }
+    public static <T> void print(T data) {
+        System.out.println(data);
+    }
+}
